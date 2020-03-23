c63638f34ef28653a75ecbe426d4bb64011afb18
Ashish <asku3000@gmail.com>
2020-03-23 20:23:34 +0530

    Ashish: Zuul Api adding

--- currency-conversion-service/src/main/java/com/practice/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/practice/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import brave.sampler.Sampler;
+
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
@@ -19,6 +21,11 @@ public class CurrencyConversionServiceApplication {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
